eb536d186a4f2ea3d190a60432934f74ae6b90fa
andrei <andrew@webupd8.org>
2013-08-01 16:22:14 +0300

    / ‘downgrade.sh’

--- downgrade.sh
+++ downgrade.sh
@@ -15,8 +15,8 @@ apt-get --ignore-missing install         \
     libva-glx1/raring                   \
     libva-tpi1/raring                   \
     libva-egl1/raring                   \
-    libva-drm1/raring                   \
-    libva-wayland1/raring               \
+#    libva-drm1/raring                   \
+#    libva-wayland1/raring               \
     vainfo/raring                       \
     libxatracker1/raring                \
     libgbm1/raring                      \
