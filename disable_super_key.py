6f6f9d2ab3a7f6921f265fc54bdaae54e1f45096
hotice <andrew@webupd8.org>
2016-08-25 11:44:08 +0300

    sync disable_super_key.py with original: https://github.com/SergKolo/sergrep/blob/master/disable_super_key.py, re-add my modifications

--- disable_super_key.py
+++ disable_super_key.py
@@ -22,16 +22,19 @@
 #     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 #     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #     DEALINGS IN THE SOFTWARE.
-
+from __future__ import print_function
 import gi
 gi.require_version('Gdk', '3.0')
 from gi.repository import  Gdk,Gio
 import subprocess
+import signal
 import time
+import sys
 
 debug = False
 
 def gsettings_get(schema,path,key):
+    """ fetches value of gsettings schema"""
     if path is None:
         gsettings = Gio.Settings.new(schema)
     else:
@@ -39,13 +42,16 @@ def gsettings_get(schema,path,key):
     return gsettings.get_value(key)
 
 def gsettings_set(schema,path,key,value):
+    """ sets value of gsettings schema """
     if path is None:
         gsettings = Gio.Settings.new(schema)
     else:
         gsettings = Gio.Settings.new_with_path(schema,path)
     return gsettings.set_string(key,value)
 
+
 def gsettings_reset(schema,path,key):
+    """ resets schema:key value to default"""
     if path is None:
         gsettings = Gio.Settings.new(schema)
     else:
@@ -53,6 +59,7 @@ def gsettings_reset(schema,path,key):
     return gsettings.reset(key)
 
 def run_cmd(cmdlist):
+    """ reusable function for running shell commands"""
     try:
         stdout = subprocess.check_output(cmdlist)
     except subprocess.CalledProcessError:
@@ -61,7 +68,9 @@ def run_cmd(cmdlist):
         if stdout:
             return stdout
 
+
 def main():
+    """ defines entry point of the program """
     screen = Gdk.Screen.get_default()
     while True:
         
@@ -74,10 +83,10 @@ def main():
         active_xid = str(screen.get_active_window().get_xid())
         wm_state =  run_cmd( ['xprop', '-root', '-notype','-id',active_xid, '_NET_WM_STATE'])  
         
-        if debug : print superkey_state,altkey_state,wm_state
+        if debug : print(superkey_state,altkey_state,wm_state)
 
         if 'FULLSCREEN' in wm_state:
-            if "Super" in superkey_state:    
+            if "Super" in  superkey_state:    
                 gsettings_set('org.compiz.unityshell', 
                               '/org/compiz/profiles/unity/plugins/unityshell/',
                               'show-launcher', 
@@ -96,9 +105,20 @@ def main():
                 gsettings_reset('org.compiz.integrated', 
                               '/org/compiz/integrated/',
                               'show-hud')
-
+    
+    
         time.sleep(0.25)
 
 
+def sigterm_handler(*args):
+    """ ensures that Super key has been reset upon exit"""
+    gsettings_reset( 'org.compiz.unityshell', 
+                     '/org/compiz/profiles/unity/plugins/unityshell/',
+                     'show-launcher')
+    if debug: print('CAUGHT SIGTERM')
+    sys.exit(0)
+
+
 if __name__ == "__main__":
+    signal.signal(signal.SIGTERM,sigterm_handler)
     main()
